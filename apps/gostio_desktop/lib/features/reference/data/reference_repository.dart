df0586cc15d75c2f32ab43fdb6ad656c3e403ab1
Isar <isar.arnautovic16@gmail.com>
2026-08-31 16:55:18 +0200

    feat: offer the countries a new city is added under

--- apps/gostio_desktop/lib/features/reference/data/reference_repository.dart
+++ apps/gostio_desktop/lib/features/reference/data/reference_repository.dart
@@ -7,6 +7,8 @@ class ReferenceRepository {
 
   final ApiClient _client;
 
+  Future<List<LookupItem>> countries() => _all('/countries');
+
   Future<List<LookupItem>> cities() => _all('/cities');
 
   Future<List<LookupItem>> accommodationTypes() => _all('/accommodation-types');
@@ -15,6 +17,18 @@ class ReferenceRepository {
       _all('/accommodation-categories');
 
   Future<List<LookupItem>> amenities() => _all('/amenities');
+
+  Future<LookupItem> addCity({
+    required String name,
+    required int countryId,
+  }) async {
+    final JsonMap body = await _client.post(
+      '/cities',
+      body: <String, dynamic>{'name': name, 'countryId': countryId},
+    );
+
+    return LookupItem.fromJson(body);
+  }
 
   Future<List<LookupItem>> _all(String path) =>
       readEveryPage<LookupItem>(_client, path, read: LookupItem.fromJson);
